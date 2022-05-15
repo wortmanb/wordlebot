b600b43201402b7639c3c85cbb6c990a2c705345
Bret Wortman <bret.wortman@damascusgrp.com>
2022-05-15 18:59:52 -0400

    Improve known letter searching
    Added KnownLetters class to encapsulate handling a dictionary where
    letters are keys and the values are arrays of integers representing the
    indices where these letters are known to _not_ be found.
    
    Then, improved the main algorithm to not only look for known letters,
    but to look for known letters in _new_ locations, and exclude those
    where the letter is in a location where it cannot be.

--- src/wordlebot.py
+++ src/wordlebot.py
@@ -12,6 +12,93 @@ HOME = os.environ.get('HOME')
 WORDLIST = f'{HOME}/git/wordlebot/data/wordlist'
 
 
+class KnownLetters:
+    """
+    Encapsulate a known letters list, also keeping track of the locations where
+    we know each word is not.
+    """
+
+    def __init__(self):
+        """
+        Constructs a new instance.
+        """
+        self.data = {}
+
+    def store(self, letter: str, index: int):
+        """
+        Store a letter and location for future reference
+
+        :param      letter:  The letter
+        :type       letter:  str
+        :param      index:   The index
+        :type       index:   int
+        """
+        if letter not in self.data.keys():
+            self.data[letter] = []
+        self.data[letter].append(index)
+
+    def keys(self) -> list[str]:
+        """
+        Return a list of all keys
+
+        :returns:   List of current keys
+        :rtype:     List of str
+        """
+        return self.data.keys()
+
+    def remove(self, letter: str):
+        """
+        Removes the specified letter.
+
+        :param      letter:  The letter
+        :type       letter:  str
+        """
+        self.data.pop(letter)
+
+    def has_letter(self, letter: str) -> bool:
+        """
+        Check to see if this letter has been seen before.
+
+        :param      letter:  The letter
+        :type       letter:  str
+
+        :returns:   True or False
+        :rtype:     bool
+        """
+        return letter in self.data.keys()
+
+    def has_letter_at_index(self, letter: str, index: int) -> bool:
+        """
+        Check to see if this letter has been seen in this index before.
+
+        :param      letter:  The letter
+        :type       letter:  str
+        :param      index:   The index
+        :type       index:   int
+
+        :returns:   True or False
+        :rtype:     bool
+        """
+        if self.has_letter(letter):
+            return index in self.data[letter]
+        return False
+
+    def indices(self, letter: str) -> list[int]:
+        """
+        Return a list of indices representing the locations where this letter
+        has already been seen.
+
+        :param      letter:  The letter
+        :type       letter:  str
+
+        :returns:   Prior locations
+        :rtype:     List of integers
+        """
+        if self.has_letter(letter):
+            return self.data[letter]
+        return []
+
+
 class Wordlebot:
     """
     This class describes a wordlebot.
@@ -25,10 +112,13 @@ class Wordlebot:
     def __init__(self, debug: bool):
         """
         Create a new wordlebot
+
+        :param      debug:  The debug
+        :type       debug:  bool
         """
         # print(f'"{partial}"')
         self.pattern = ['.'] * 5
-        self.known = []
+        self.known = KnownLetters()
         self.bad = []
         self.debug = debug
         with open(WORDLIST, 'r') as input:
@@ -37,6 +127,9 @@ class Wordlebot:
     def help_msg(self):
         """
         Return a help/usage message.
+
+        :returns:   { description_of_the_return_value }
+        :rtype:     { return_type_description }
         """
 
         return """
@@ -54,77 +147,83 @@ Next guesses: cling, clink, clung, count, icing
 
 """
 
+    def log(self, message: str):
+        """
+        Internal logging method
+
+        :param      message:  The message
+        :type       message:  String
+        """
+        if self.debug:
+            print(message)
+
     def guess(self, guess: str):
         """
         Handle this guess by adding each letter to the bad list for now. They
         can be removed during assessment of the response
-        
+
         :param      guess:  The guess
         :type       guess:  str
         """
         for letter in guess:
-            self.bad.append(letter)
+            if not self.known.has_letter(letter):
+                self.bad.append(letter)
 
     def assess(self, response: str):
         """
         Assess the last response. Add any new greens to the pattern and any new
         yellows to the known list. This does not yet do anything to track
         letters known to not be part of the solution.
-        
+
         :param      response:        The response
         :type       response:        str
-        
+
         :raises     AssertionError:  { exception_description }
         """
         assert len(response) == 5
         assert re.match(VALIDATION, response)
         for idx, letter in enumerate(list(response)):
             if re.match('[a-z]', letter):
-                self.known.append(letter)
+                self.known.store(letter, idx)
                 if letter in self.bad:
                     self.bad.remove(letter)
             if re.match('[A-Z]', letter):
                 letter = letter.lower()
                 self.pattern[idx] = letter
-                if letter in self.known:
+                if letter in self.known.keys():
                     self.known.remove(letter)
                 if letter in self.bad:
                     self.bad.remove(letter)
-        if self.debug:
-            print(f'pattern: {self.pattern}')
-            print(f'known: {self.known}')
-            print(f'bad: {self.bad}')
-
+        self.log(f'pattern: {self.pattern}')
+        self.log(f'known: {self.known}')
+        self.log(f'bad: {self.bad}')
 
     def solve(self, response: str) -> list[str]:
         """
         Look for words that make good candidates given this response and prior
         ones as well.
-        
+
         :param      response:  The last response
         :type       response:  str
-        
+
         :returns:   List of matching words
         :rtype:     list[str]
         """
         self.assess(response)
         candidates = []
         for word in self.wordlist:
-            if self.debug:
-                print(f'Considering {word}')
+            self.log(f'Considering {word}')
             # Does it match the pattern?
             pattern = ''.join(self.pattern)
             if not re.match(pattern, word):
-                if self.debug:
-                    print(f' {word} does not match {pattern}')
+                self.log(f' {word} does not match {pattern}')
                 # self.wordlist.remove(word)
                 continue
             # Does it contain any letters in the bad letter list?
             matched = False
             for letter in word:
                 if letter in self.bad:
-                    if self.debug:
-                        print(f' {word} contains "{letter}" but shouldn\'t')
+                    self.log(f' {word} contains "{letter}" but shouldn\'t')
                     matched = True
                     break
             if matched:
@@ -132,32 +231,36 @@ Next guesses: cling, clink, clung, count, icing
                 continue
             # Now, are all the letters in the known list present in the word?
             violated = False
-            for letter in self.known:
+            for letter in self.known.keys():
                 if letter not in word:
-                    if self.debug:
-                        print(f' {word} does not contain "{letter}"')
+                    self.log(f' {word} does not contain "{letter}"')
+                    violated = True
+                    break
+                index = word.find(letter)
+                if self.known.has_letter_at_index(letter, index):
+                    self.log(f' {word} contains {letter} at {index}')
                     violated = True
                     break
             if violated:
-                if self.debug:
-                    print(f'Removing {word}')
+                self.log(f'Removing {word}')
                 # self.wordlist.remove(word)
             else:
                 candidates.append(word)
-        if self.debug:
-            print(f'candidates: {candidates}')
+        self.log(f'candidates: {candidates}')
         self.wordlist = candidates
         return candidates
+
 
 def main():
     """
     Main function
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--quiet", action="store_false", dest="usage", 
-        default=True, help="Don't print the handy dandy usage message")
+    parser.add_argument("--quiet", action="store_false", dest="usage",
+                        default=True,
+                        help="Don't print the handy dandy usage message")
     parser.add_argument("--debug", "-d", action="store_true", dest="debug",
-        default=False, help="Print extra debugging output")
+                        default=False, help="Print extra debugging output")
     args = parser.parse_args()
 
     wb = Wordlebot(args.debug)
@@ -173,7 +276,8 @@ def main():
         count = len(solutions)
         print(f'There are {count} possible guesses: {sol}')
         if len(solutions) <= 1:
-            break 
+            break
+
 
 if __name__ == '__main__':
     main()
